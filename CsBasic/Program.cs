1a46909365ef0e3915de7ba4e49cb6cd33ac003f
MoonHyekyung <moonsoft02@gmail.com>
2020-04-28 14:21:52 +0900

    C# 기본 문법 연습

--- CsBasic/Program.cs
+++ CsBasic/Program.cs
@@ -10,6 +10,32 @@ namespace CsBasic
     {
         static void Main(string[] args)
         {
+            var name = "문" + "혜" + "경";
+            Console.Write("Hello C#");
+            int add = 2;
+            int Alpha;
+            // 주석
+            /*
+             * 
+             * 여러 줄 주석
+             * 
+             */
+            Console.WriteLine("HelloWorld");
+            Console.Write("HelloWorld");
+
+
+            Console.WriteLine(7 % 3);
+
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("\"\"\"");
+
+            Console.WriteLine("가나" + "다라마바" + "사아" + "자차카" + "타파하");
+
+            Console.WriteLine("안녕하세요"[100]);
+
+            Console.WriteLine(true);
+            Console.WriteLine(false);
         }
     }
 }
